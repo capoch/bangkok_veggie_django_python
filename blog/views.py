5a75e139b62a79b8161412c7334edc69c44845b5
capoch <buemplizer@gmail.com>
2017-01-23 14:54:48 +0800

    trydjango19 upto 27

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -30,12 +31,27 @@ def posts_detail(request,pk):
 
 
 def posts_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 10) # Show 10 contacts per page
+    page_request_var = 'abc'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
+        "title":"BangkokVeg - Streetfood Dilemma",
         "object_list": queryset,
-        "title":"Liste mit Titel und Shice",
+        "page_request_var": page_request_var,
     }
-    return render(request,'index.html',context)
+    return render(request,'post_list.html',context)
+
+
 
 def posts_edit(request, pk=None):
     post = get_object_or_404(Post,id=pk)
